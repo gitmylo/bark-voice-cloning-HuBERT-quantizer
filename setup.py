c982344158b811f1056a59ea6c285b6e4501a631
Roberts Slisans <roberts.slisans@gmail.com>
2023-07-21 13:30:55 +0300

    bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='bark_hubert_quantizer',
-    version='0.0.3',
+    version='0.0.4',
     packages=['bark_hubert_quantizer'],
     install_requires=[
         'audiolm-pytorch==1.1.4',
